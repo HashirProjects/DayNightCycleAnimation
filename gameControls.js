b80e5956a66a323785fa082a67485bd4bbd7816b
HashirProjects <hashirrana2001@gmail.com>
2021-06-12 19:52:06 +0100

    added night cycle

--- gameControls.js
+++ gameControls.js
@@ -1,17 +1,30 @@
-let day = {
+let dayNightCycle = {
 
     sun: document.getElementById('sun'),
+    game: document.getElementById('gameWindow'),
     x:0,
+    currentColor: "yellow",
 
     moveSun: function () {
         this.x = this.x + 1;
         y= parseInt((1/625)*this.x*this.x - (8/5)*this.x + 400);
-        sun.style.left = this.x + "px";
-        sun.style.top = y + "px";
+        this.sun.style.left = this.x + "px";
+        this.sun.style.top = y + "px";
     
-        if (parseInt(sun.style.left) > 975) {
-            sun.style.left=0;
-            sun.style.top=400;
+        if (this.x > 975) {
+
+            if (this.currentColor == "yellow") {
+                this.currentColor = "gray";
+                this.game.style.backgroundColor = "black";
+            } else {
+                this.currentColor = "yellow";
+                this.game.style.backgroundColor = "lightblue";
+            }
+
+            this.sun.style.backgroundColor = this.currentColor;
+
+            this.sun.style.left=0;
+            this.sun.style.top=400;
             this.x=0;
         }
     },
@@ -21,8 +34,7 @@ let day = {
             this.moveSun();
         }, 16);
     }
-
 }
 
-day.endlessLoop();
+dayNightCycle.endlessLoop();
 
